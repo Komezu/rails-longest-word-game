fd6de5ecd9d8ee1ec953c8c26fa9cb3991ab9fe1
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-07 22:07:50 -0500

    added score feature (with reset) to game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,13 @@ require 'open-uri'
 
 # Controller for the game
 class GamesController < ApplicationController
+  before_action :set_score
+
+  def reset
+    session[:score] = nil
+    redirect_to new_path
+  end
+
   def new
     alphabet = ('A'..'Z').to_a
     @letters = []
@@ -10,27 +17,42 @@ class GamesController < ApplicationController
 
   def score
     @grid = params[:letters].chars
-    @word = params[:word].upcase
+    @word = params[:word].strip.upcase
 
-    @message = if !grid_valid?(@grid, @word)
-                 'grid_fail'
-               elsif !english_valid?(@word)
-                 'english_fail'
-               else
-                 'success'
-               end
+    word_data = parse_word(@word)
+
+    if !grid_valid?(@grid, @word)
+      @fail = 'grid_fail'
+    elsif !word_data['found']
+      @fail = 'english_fail'
+    else
+      @score = update_score(word_data)
+    end
   end
 
   private
+
+  def set_score
+    if session[:score].nil?
+      @score = 0
+      session[:score] = 0
+    else
+      @score = session[:score]
+    end
+  end
+
+  def parse_word(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    JSON.parse(URI.parse(url).open.read)
+  end
 
   def grid_valid?(grid, word)
     word = word.chars
     word.all? { |char| word.count(char) <= grid.count(char) }
   end
 
-  def english_valid?(word)
-    url = "https://wagon-dictionary.herokuapp.com/#{word}"
-    word_data = JSON.parse(URI.parse(url).open.read)
-    word_data['found'] == true
+  def update_score(word_data)
+    session[:score] += word_data['length']
+    session[:score]
   end
 end
